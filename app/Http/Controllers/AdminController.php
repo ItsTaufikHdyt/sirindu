9fd08bf77a705a7b4ac69a66b732e724690afeee
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2022-12-19 12:55:12 +0800

    Perbaikan algoritma pengkategorian

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -158,7 +158,7 @@ ANAK
                 $err = "Nilai Berat Badan tidak normal";
             }
             $imt_u = DB::table('z_score')
-                ->select('id', 'm3sd as a1', 'm2sd as b1', '2sd as c1')
+                ->select('id', 'm3sd as a1', 'm2sd as b1', '1sd as c1', '2sd as d1', '3sd as e1')
                 ->where([
                     'var' => $var,
                     'acuan' => $umur,
@@ -166,14 +166,14 @@ ANAK
                     'jenis_tbl' => 1,
                 ])->get();
             $bb_u = DB::table('z_score')
-                ->select('id', 'm3sd as a2', 'm2sd as b2', '2sd as c2')
+                ->select('id', 'm3sd as a2', 'm2sd as b2', '1sd as c2', '2sd as d2', '3sd as e2')
                 ->where([
                     'acuan' => $umur,
                     'jk' => $jk,
                     'jenis_tbl' => 2,
                 ])->get();
             $tb_u = DB::table('z_score')
-                ->select('id', 'm3sd as a3', 'm2sd as b3', '2sd as c3')
+                ->select('id', 'm3sd as a3', 'm2sd as b3', '1sd as c3', '2sd as d3', '3sd as e3')
                 ->where([
                     'var' => $var,
                     'acuan' => $umur,
@@ -181,7 +181,7 @@ ANAK
                     'jenis_tbl' => 3,
                 ])->get();
             $bt_tb = DB::table('z_score')
-                ->select('id', 'm3sd as a4', 'm2sd as b4', '2sd as c4')
+                ->select('id', 'm3sd as a4', 'm2sd as b4', '1sd as c4', '2sd as d4', '3sd as e4')
                 ->where([
                     'var' => $var,
                     'acuan' => $tinggi,
@@ -190,43 +190,51 @@ ANAK
                 ])->get();
 
             if ($bmi < $imt_u[0]->a1) {
-                $s1 = "Sangat Kurus";
+                $s1 = "Gizi buruk (severely wasted)";
             } elseif ($bmi >= $imt_u[0]->a1 && $bmi < $imt_u[0]->b1) {
-                $s1 = "Kurus";
+                $s1 = "Gizi kurang (wasted)";
             } elseif ($bmi >= $imt_u[0]->b1 && $bmi <= $imt_u[0]->c1) {
-                $s1 = "Normal";
+                $s1 = "Gizi baik (normal)";
+            }elseif ($bmi > $imt_u[0]->c1 && $bmi <= $imt_u[0]->d1) {
+                $s1 = "Berisiko gizi lebih (possible risk of overweight";
+            }elseif ($bmi > $imt_u[0]->d1 && $bmi <= $imt_u[0]->e1) {
+                $s1 = "Gizi lebih (overweight)";
             } else {
-                $s1 = "Gemuk";
+                $s1 = "Obesitas (obese)";
             }
 
             if ($berat < $bb_u[0]->a2) {
-                $s2 = "Gizi Buruk";
+                $s2 = "Berat badan sangat kurang (severely underweight)";
             } elseif ($berat >= $bb_u[0]->a2 && $berat < $bb_u[0]->b2) {
-                $s2 = "Gizi Kurang";
+                $s2 = "Berat badan kurang (underweight)";
             } elseif ($berat >= $bb_u[0]->b2 && $berat <= $bb_u[0]->c2) {
-                $s2 = "Gizi Baik";
+                $s2 = "Berat badan normal";
             } else {
-                $s2 = "Gizi Lebih";
+                $s2 = "Risiko Berat badan lebih";
             }
 
             if ($tinggi < $tb_u[0]->a3) {
-                $s3 = "Sangat Pendek";
+                $s3 = "Sangat pendek (severely stunted)";
             } elseif ($tinggi >= $tb_u[0]->a3 && $tinggi < $tb_u[0]->b3) {
-                $s3 = "Pendek";
-            } elseif ($tinggi >= $tb_u[0]->b3 && $tinggi <= $tb_u[0]->c3) {
+                $s3 = "Pendek (stunted)";
+            } elseif ($tinggi >= $tb_u[0]->b3 && $tinggi <= $tb_u[0]->e3) {
                 $s3 = "Normal";
             } else {
                 $s3 = "Tinggi";
             }
             try {
                 if ($berat < $bt_tb[0]->a4) {
-                    $s4 = "Sangat Kurus";
+                    $s4 = "Gizi buruk (severely wasted";
                 } elseif ($berat >= $bt_tb[0]->a4 && $berat < $bt_tb[0]->b4) {
-                    $s4 = "Kurus";
+                    $s4 = "Gizi kurang (wasted)";
                 } elseif ($berat >= $bt_tb[0]->b4 && $berat <= $bt_tb[0]->c4) {
-                    $s4 = "Normal";
+                    $s4 = "Gizi baik (normal)";
+                } elseif ($berat > $bt_tb[0]->c4 && $berat <= $bt_tb[0]->d4) {
+                    $s4 = "Berisiko gizi lebih (possible risk of overweight)";
+                } elseif ($berat > $bt_tb[0]->d4 && $berat <= $bt_tb[0]->e4) {
+                    $s4 = "Gizi lebih (overweight)";
                 } else {
-                    $s4 = "Gemuk";
+                    $s4 = "Obesitas (obese)";
                 }
             } catch (\Exception $e) {
                 // dump('culprit : '.$key.', Error : '.$e->getMessage());
